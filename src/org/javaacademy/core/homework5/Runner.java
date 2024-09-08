08ef4f7417bd0bddc130f51234f7d2138500156a
Dmitry <badl1970@mail.ru>
2024-09-08 21:08:03 +0300

    fix bugs in homework 5

--- src/org/javaacademy/core/homework5/Runner.java
+++ src/org/javaacademy/core/homework5/Runner.java
@@ -1,14 +1,20 @@
 package org.javaacademy.core.homework5;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import org.javaacademy.core.homework5.ex2.Computer;
+import org.javaacademy.core.homework5.ex2.ComputerSpy;
+
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Runner {
 
-    public static final int COUNTRY_NAME = 0;
-    public static final int SUM_AMOUNT = 1;
+    private static final int COUNTRY_FIELD_INDEX = 0;
+    private static final int MONEY_FIELD_INDEX = 1;
+    private static final int MINIMUM_FIELDS_COUNT = 2;
+    private static final String DELIMETER = ";";
+
 
     public static void main(String[] args) {
         ex1();
@@ -18,97 +24,71 @@ public class Runner {
     private static void ex1() {
 
         String[] countries = {"Россия", "Франция", "Китай", "Япония", "Турция"};
+        BigDecimal[] moneyForCountry = new BigDecimal[countries.length];
+        Arrays.fill(moneyForCountry, BigDecimal.ZERO);
+
+
         String filename = "donation.csv";
 
-        BigDecimal sumRussia = BigDecimal.ZERO;
-        BigDecimal sumFrance = BigDecimal.ZERO;
-        BigDecimal sumChina = BigDecimal.ZERO;
-        BigDecimal sumJapan = BigDecimal.ZERO;
-        BigDecimal sumTurkey = BigDecimal.ZERO;
+        try (Scanner scanner = new Scanner(Runner.class.getClassLoader().getResourceAsStream(filename));) {
+            readAllLines(scanner, moneyForCountry, countries);
+        }
 
-        Scanner scanner = new Scanner(Runner.class.getClassLoader().getResourceAsStream(filename));
+        System.out.println("Список пожертвований по странам");
+        for (int i = 0; i < countries.length; i++) {
+            System.out.printf("%s - %s\n", countries[i], moneyForCountry[i]);
+        }
+    }
 
-        while (scanner.hasNext()) {
-            String[] donationsSum = scanner.nextLine().replace(",", ".")
-                    .replace(";", ",").split(",");
-            if ((donationsSum[1].matches("-?\\d+(\\.\\d+)?"))) {
-                switch (donationsSum[COUNTRY_NAME]) {
-                    case "Россия":
-                        sumRussia = sumRussia.add(BigDecimal.valueOf(Double.parseDouble(donationsSum[SUM_AMOUNT])));
-                        break;
-                    case "Франция":
-                        sumFrance = sumFrance.add(BigDecimal.valueOf(Double.parseDouble(donationsSum[SUM_AMOUNT])));
-                        break;
-                    case "Китай":
-                        sumChina = sumChina.add(BigDecimal.valueOf(Double.parseDouble(donationsSum[SUM_AMOUNT])));
-                        break;
-                    case "Япония":
-                        sumJapan = sumJapan.add(BigDecimal.valueOf(Double.parseDouble(donationsSum[SUM_AMOUNT])));
-                        break;
-                    case "Турция":
-                        sumTurkey = sumTurkey.add(BigDecimal.valueOf(Double.parseDouble(donationsSum[SUM_AMOUNT])));
-                        break;
-                }
+    private static void readAllLines(Scanner scanner, BigDecimal[] moneyForCountry, String[] countries) {
+        scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String text = scanner.nextLine();
+            BigDecimal moneyFromCountry = getNumberFromLine(text);
+            addMoneyToCountryResult(countries, moneyForCountry, moneyFromCountry, getCountryName(text));
+        }
+    }
+
+    private static BigDecimal getNumberFromLine(String textLine) {
+        String[] fields = textLine.split(DELIMETER);
+        if (fields.length >= MINIMUM_FIELDS_COUNT && isNumber(fields[MONEY_FIELD_INDEX])) {
+            String convertedMoney = fields[MONEY_FIELD_INDEX].replace(",", ".");
+            return new BigDecimal(convertedMoney);
+        }
+        return BigDecimal.ZERO;
+    }
+
+    private static boolean isNumber(String text) {
+        Pattern pattern = Pattern.compile("^\\d+,\\d+$");
+        return pattern.matcher(text).find();
+    }
+
+    private static String getCountryName(String textLine) {
+        String[] fields = textLine.split(DELIMETER);
+        if (fields.length == 0) {
+            return "";
+        }
+        return fields[COUNTRY_FIELD_INDEX];
+    }
+
+    private static void addMoneyToCountryResult(String[] countries, BigDecimal[] countryResults,
+                                                BigDecimal moneyForCountry,
+                                                String countryName) {
+
+        for (int i = 0; i < countries.length; i++) {
+            if (countries[i].equals(countryName)) {
+                countryResults[i] = countryResults[i].add(moneyForCountry);
             }
         }
-
-        System.out.printf("%s - %.2f\n", "Россия", sumRussia);
-        System.out.printf("%s - %.2f\n", "Франция", sumFrance);
-        System.out.printf("%s - %.2f\n", "Китай", sumChina);
-        System.out.printf("%s - %.2f\n", "Япония", sumJapan);
-        System.out.printf("%s - %.2f\n", "Турция", sumTurkey);
     }
-
 
     private static void ex2() {
-        //Сделать компьютерного шпиона
-        //Смысл задания - отслеживать действия реального объекта и записывать их в лог
-        //Создать компьютер:
-        //1. Компьютер хранит в себе пользователя, который вошел в компьютер
-        //2. Компьютер умеет:
-        //2.1. Делать вход пользователя (на вход имя пользователя).
-        // Изменяет пользователя, который зашел в компьютер
-        //2.2 Делать выход пользователя. Удаляет пользователя из системы
-        //2.3. Отправляет сообщение (на вход текст сообщения).
-        // Печатает на экран "сообщение: {текст сообщения}".
-
-        //Создать компьютерного шпиона, который наследуется от компьютера,
-        // при этом хранит в себе обычный компьютер
-        //Сделать так, чтобы при вызове методов компьютерного шпиона:
-        //1. При входе пользователя делалась запись в comp.log "Пользователь {имя пользователя} вошел"
-        // и вызывался метод компьютера.
-        //2. При выходе делалась запись в comp.log "Пользователь вышел"
-        //3. При отправке сообщения, делалась запись в comp.log:
-        // "Пользователь {имя пользователя} отправил сообщение {текст сообщения}"
-        // и вызвался метод компьютера.
-
-        //Создать Runner
-        //1. В нем создать компьютер.
-        //2. Создать шпиона с компьютером из пункта 1
-        //3. Сделать вход пользователя, отправку сообщения, выход пользователя через компьютерного шпиона.
-        //4. Проверить наличие логов в comp.log
-        // (файл должен быть внутри репозитория и отправлен в удаленное репо)
-
-
-        Computer.User user = new Computer.User("Иван");
         Computer computer = new Computer();
         ComputerSpy computerSpy = new ComputerSpy(computer);
-        try {
-            computerSpy.userEnter(user.getUserName());
-            computerSpy.sendMessage("Отправка сообщения");
-            computerSpy.userExit("Сергей");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        String filename = "comp.log";
-        Scanner scanner = new Scanner(Runner.class.getClassLoader().getResourceAsStream(filename));
-//        computerSpy.readFile();
-//
-//
+        computerSpy.login("Юрий");
+        computerSpy.sendMessage("Ура");
+        computerSpy.logout();
     }
-
-
 }
 
 
